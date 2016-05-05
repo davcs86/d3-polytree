46b5bee877f772b3066b871550fdfd9e05bbad0d
David Castillo <davcs86@gmail.com>
2016-05-05 10:18:11 -0500

    fix: arrow path calculation

--- dist/d3-simple-networks.js
+++ dist/d3-simple-networks.js
@@ -8,7 +8,7 @@
  *
  * Source Code: https://github.com/davcs86/d3-simple-networks
  *
- * Date: 2016-05-04
+ * Date: 2016-05-05
  */
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.D3SimpleNetwork = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(_dereq_,module,exports){
 'use strict';
@@ -365,10 +365,28 @@ SimpleNetwork.prototype.restart = function(recalculate){
       .attr('x2', function(d) { return d.target.x; })
       .attr('y2', function(d) { return d.target.y; })
       .attr('d', function(d) {
-        var linePath = "M " + (d.source.x + 27) + "," + d.source.y;
+
+        if (d.source.x>d.target.x){
+          var linePath = "M " + (d.source.x - 27) + "," + d.source.y;
+        } else {
+          var linePath = "M " + (d.source.x + 27) + "," + d.source.y;
+        }
         if (d.target.y == d.source.y){
           // straight line
-          linePath += 'L'+(d.target.x - 27)+','+d.target.y;
+          if (d.source.x>d.target.x){
+            linePath += 'L'+(d.target.x + 27)+','+d.target.y;
+          } else {
+            linePath += 'L'+(d.target.x - 27)+','+d.target.y;
+          }
+        } else if (Math.abs(d.target.y - d.source.y) < 50){
+          // angular line (forward)
+          linePath += 'L'+(d.source.x + ((d.target.x - d.source.x)/2))+','+d.source.y;
+          linePath += 'L'+(d.source.x + ((d.target.x - d.source.x)/2))+','+d.target.y;
+          if (d.source.x>d.target.x){
+            linePath += 'L'+(d.target.x + 27)+','+d.target.y;
+          } else {
+            linePath += 'L'+(d.target.x - 27)+','+d.target.y;
+          }
         } else {
           // angular line
           linePath += 'L'+d.target.x+','+d.source.y;
