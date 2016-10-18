8cd0d198e69747589652e8fd9daef6242d13f907
David Castillo <davcs86@gmail.com>
2016-10-18 11:01:09 -0500

    GotoNodePosition function added

--- dist/d3-simple-networks.js
+++ dist/d3-simple-networks.js
@@ -8,7 +8,7 @@
  *
  * Source Code: https://github.com/davcs86/d3-simple-networks
  *
- * Date: 2016-10-17
+ * Date: 2016-10-18
  */
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.D3SimpleNetwork = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(_dereq_,module,exports){
 'use strict';
@@ -318,7 +318,7 @@ SimpleNetwork.prototype.initSVG = function(){
     this.drawGridLines();
   }
 
-  var zoom = d3js
+  var zoom = this._zoom = d3js
     .behavior
     .zoom()
     .scaleExtent([0.05, 10])
@@ -810,6 +810,29 @@ SimpleNetwork.prototype.getNodesPosition = function(){
     scale: canvasTransform.scale[0],
     nodes: this.nodes
   };
+};
+
+SimpleNetwork.prototype.goToNodePosition = function(nodeId){
+  // update data with the last position
+  this.tick();
+  var node = this.nodes[nodeId],
+      height = this.options.height * 1.0,
+      width = this.options.width * 1.0,
+      that = this;
+  if (!isUndefined(node)){
+    this.SVG
+      .transition()
+      .duration(1800)
+      .call(function () {
+        that._zoom
+          .translate([
+            (-1 * node.position.x * 1.15 + (width / 2)),
+            (-1 * node.position.y * 1.15 + (height / 2))
+          ])
+          .scale(1.15)
+          .event(this); // apply zoom
+      });
+  }
 };
 
 SimpleNetwork.prototype.getFloatingLabelsPosition = function(){
